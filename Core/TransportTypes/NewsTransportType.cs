af803edb80d4c97c59c099684755c32639eb29a3
Nagibator3000 <kamvalentin@gmail.com>
2014-09-11 00:44:38 +0400

    Добавил вывод последней новости в блоке справа

---
+++ Core/TransportTypes/NewsTransportType.cs
@@ -0,0 +1,25 @@
+﻿namespace Core.TransportTypes
+{
+    public class NewsTransportType
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public string Text { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public string Image { get; set; }
+    }
+}
