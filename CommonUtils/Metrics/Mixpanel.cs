5b68c7f0f91e2bccd284d27fe320142726820799
nvcccu <qwer@qwer.qwer>
2014-09-22 23:00:07 +0400

    заготовочка для mixpanel

---
+++ CommonUtils/Metrics/Mixpanel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+
+namespace CommonUtils.Metrics {
+    public class Mixpanel {
+        private static NLog.Logger _logger = NLog.LogManager.GetCurrentClassLogger();
+
+        private static AsyncCallback CallBack(HttpWebRequest request) {
+            return obj => {
+                try {
+                    if (request != null) {
+                        request.GetResponse().Close();
+                    }
+                } catch (Exception exception) {
+                    _logger.Info("Что-то пошло не так при отправке запроса в Mixpanel" +exception);
+                }
+            };
+        }
+
+        public void PostEvent(long guestId, int actionId, Dictionary<string, string> additionalParams) {
+            
+        }
+    }
+}
