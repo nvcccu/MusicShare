4f14e7412cebfbc6ada96e80fedeaafb3879373d
nvcccu <qwer@qwer.qwer>
2015-07-29 15:51:58 +0400

    Один урок с индивидуальными для упражнений метрономами.

---
+++ MusicShare/Controllers/LessonController.cs
@@ -0,0 +1,9 @@
+﻿using System.Web.Mvc;
+
+namespace MusicShareWeb.Controllers {
+    public class LessonController : BaseController {
+        public ActionResult Index(int? id) {
+            return View("Index", BaseModel);
+        }
+    }
+}
---
+++ MusicShare/Models/Lesson/ExerciseModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MusicShareWeb.Models.Lesson
+{
+    public class ExerciseModel
+    {
+        public string NotesImageUrl { get; set; }
+    }
+}
