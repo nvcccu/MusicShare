381ddd991ba84c3b768e5912957eb7d018b95ffe
nvcc <nvcc@nvcc.nvcc>
2015-08-22 20:54:07 +0300

    Доделанная главная страница самоучителя. Выкинул из навигации в хедере лишние ссылки.

--- MusicShare/Models/Lesson/LessonListModel.cs
+++ MusicShare/Models/Lesson/LessonListModel.cs
@@ -7,10 +7,12 @@ namespace MusicShareWeb.Models.Lesson {
     public class LessonListModel : BaseModel {
         public List<GuitarTechniqueDto> GuitarTechniques { get; set; }
         public List<LessonDto> Lessons { get; set; }
+        public List<PlanDto> Plans { get; set; }
        
         public LessonListModel(BaseModel baseModel) : base(baseModel) {
             GuitarTechniques = ServiceManager<IBusinessLogic>.Instance.Service.GetAllGuitarTechniques();
             Lessons = ServiceManager<IBusinessLogic>.Instance.Service.GetAllLessons();
+            Plans = CurrentUser != null ? ServiceManager<IBusinessLogic>.Instance.Service.GetAllUsersPlans(CurrentUser.Id) : new List<PlanDto>();
         }
     }
 }
