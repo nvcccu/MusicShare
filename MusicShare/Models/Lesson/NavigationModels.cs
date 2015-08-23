f330bab5ba6d626d3e1244ba766565dd99ee5f8b
nvcc <nvcc@nvcc.nvcc>
2015-08-23 11:41:29 +0300

    Поправил багу с авторизацией в FireFox'е, Вынес боковую навигацию по планам в компоненту knockout'а

---
+++ MusicShare/Models/Lesson/NavigationModels.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using BusinessLogic.Interfaces;
+using CommonUtils.ServiceManager;
+using Core.TransportTypes;
+
+namespace MusicShareWeb.Models.Lesson {
+    public class LessonNavigationModel {
+        public List<GuitarTechniqueDto> GuitarTechniques { get; set; }
+        public List<LessonDto> Lessons { get; set; }
+       
+        public LessonNavigationModel() {
+            GuitarTechniques = ServiceManager<IBusinessLogic>.Instance.Service.GetAllGuitarTechniques();
+            Lessons = ServiceManager<IBusinessLogic>.Instance.Service.GetAllLessons();
+        }
+    }
+
+    public class PlanNavigationModel {
+        public List<PlanDto> Plans { get; set; }
+       
+        public PlanNavigationModel(int? accoutnId = null) {
+            Plans = accoutnId.HasValue
+                ? ServiceManager<IBusinessLogic>.Instance.Service.GetAllUsersPlans(accoutnId.Value)
+                : new List<PlanDto>();
+        }
+    }
+}
